1497ebbacf0b802512a333a03cb27cbe017f289f
Cutean Bogdan Mihai <bogdancutean@proton.me>
2024-11-08 21:18:54 +0200

    Edit [Data Processing Service] - Enhance connectToRabbitMQ for Docker context

--- services/data-processing-service/src/queue.ts
+++ services/data-processing-service/src/queue.ts
@@ -4,10 +4,30 @@ import { processTrackingData } from "./processes/processTrackingData";
 
 let channel: Channel;
 
-export const connectToRabbitMQ = async (): Promise<void> => {
-  const connection: Connection = await amqplib.connect(RABBITMQ_URL);
-  channel = await connection.createChannel();
-  console.log("Data Processing Service - Connected to RabbitMQ");
+export const connectToRabbitMQ = async (
+  retries = 5,
+  delay = 5000
+): Promise<void> => {
+  while (retries > 0) {
+    try {
+      const connection: Connection = await amqplib.connect(RABBITMQ_URL);
+      channel = await connection.createChannel();
+      console.log("Data Processing Service - Connected to RabbitMQ");
+      return;
+    } catch (error) {
+      console.error(
+        `Failed to connect to RabbitMQ. Retrying in ${delay / 1000} seconds...`,
+        error
+      );
+      retries -= 1;
+      if (retries === 0) {
+        throw new Error(
+          "Could not connect to RabbitMQ after multiple attempts"
+        );
+      }
+      await new Promise((res) => setTimeout(res, delay));
+    }
+  }
 };
 
 export const consumeMessages = async (): Promise<void> => {
