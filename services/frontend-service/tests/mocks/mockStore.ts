5a6f7c85275b2228cc33aaa2e7d232dfbb5fbf22
Cutean Bogdan Mihai <bogdancutean@proton.me>
2024-11-08 12:49:14 +0200

    Edit [Frontend Service] - Small edit mockStore

--- services/frontend-service/tests/mocks/mockStore.ts
+++ services/frontend-service/tests/mocks/mockStore.ts
@@ -1,11 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "@/redux/slices/authSlice";
+import websitesReducer from "@/redux/slices/websitesSlice";
 import { RootState } from "@/redux/store";
 
 export const createMockStore = (initialState?: Partial<RootState>) => {
   return configureStore({
     reducer: {
       auth: authReducer,
+      websites: websitesReducer,
     },
     preloadedState: initialState as RootState,
     middleware: (getDefaultMiddleware) =>
